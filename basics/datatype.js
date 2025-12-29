ad4067e98e0b2a89a45891f7d2223d66f466cb4e
kratidwivedi123 <kratidwivedi165@gmail.com>
2025-12-29 17:38:18 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,16 @@
+let LoggedIn = false
+let state;
+
+// number =>2 to power 53
+//bignt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//underfined =>
+// symbol
+
+console.log(typeof "krari")
+console.log(typeof undefined);     // undefined
+console.log(typeof null);         //object
+
+
