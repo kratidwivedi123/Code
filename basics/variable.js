da9218304a7dcca81a4698832d8689902a6a86c9
kratidwivedi123 <kratidwivedi165@gmail.com>
2025-12-29 17:37:43 +0000

    variables

---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountId= "1233443"
+let accountEmail="krti@fd.com"
+var accountPassword="77896"
+let accountCity="kanpur"
+let accountState;
+
+/*
+Prefer not to use var
+because of issues in block scope and function scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
